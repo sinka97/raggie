c623becbd4e08047c5f0c85a30bfc028c0405b3e
Thant Sin Khine Aung <yourssincerelysin@outlook.com>
2024-07-23 22:50:40 +0800

    fixing pip install issues

--- src/rag_utils.py
+++ src/rag_utils.py
@@ -1,16 +1,17 @@
 # Overwrite Old pysqlite3
-__import__('pysqlite3')
 import sys
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+if 'pysqlite3' in sys.modules:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
 import chromadb
 from chromadb.config import DEFAULT_TENANT, DEFAULT_DATABASE, Settings
 import pandas as pd
 
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.document_loaders import PyMuPDFLoader
 from langchain_chroma import Chroma
 from langchain_experimental.text_splitter import SemanticChunker
+from langchain_community.document_loaders import PyMuPDFLoader
 
 
 def get_embedding_model(model_name=None,device=None,normalize=False):
@@ -29,16 +30,13 @@ def get_embedding_model(model_name=None,device=None,normalize=False):
     )
     return embed_fn
 
-def embed_and_store_document(chromadb_ip,doc,collection_name,embed_fn):
+def embed_and_store_document(chromadb_ip,file_name,collection_name,embed_fn):
     # Instantiate connection to client
     client = chromadb.HttpClient( 
         host=chromadb_ip, 
         port=8000, 
         ssl=False, 
-        headers=None, 
-        settings=Settings(allow_reset=False, anonymized_telemetry=False), 
-        tenant=DEFAULT_TENANT, 
-        database=DEFAULT_DATABASE, 
+        settings=Settings(anonymized_telemetry=False)
     )
     # Check if connection is live
     try:
@@ -46,11 +44,12 @@ def embed_and_store_document(chromadb_ip,doc,collection_name,embed_fn):
     except Exception as e:
         return "error", f"An unexpected error occurred: {e}"
     text_splitter = SemanticChunker(embed_fn)
-    splits = text_splitter.split_documents(doc)
+    pdf_data = PyMuPDFLoader(file_name, extract_images=True).load()
+    splits = text_splitter.split_documents(pdf_data)
     Chroma.from_documents(splits, client=client, collection_name=collection_name, embedding=embed_fn)
     return "success", f"Document stored to remote chromadb collection: {collection_name}."
 
-def load_document(chromadb_ip, collection_name, embed_fn):
+def load_col_from_chroma(chromadb_ip, collection_name, embed_fn):
     # Instantiate connection to client
     client = chromadb.HttpClient( 
         host=chromadb_ip, 
@@ -63,13 +62,13 @@ def load_document(chromadb_ip, collection_name, embed_fn):
     )
     # Check if collection exists
     try:
-        client.get_collection(collection_name)
+        client.get_or_create_collection(collection_name)
     except Exception as e:
         return "error", f"An unexpected error occurred: {e}"
     col = Chroma(client=client, embedding_function=embed_fn, collection_name=collection_name)
     return col
 
-def load_excel(excel_file,sheet_name):
+def upload_excel(excel_file,sheet_name):
     dfDict = pd.read_excel(excel_file, sheet_name=sheet_name)
     dfs = []
     dfs.append(dfDict)
